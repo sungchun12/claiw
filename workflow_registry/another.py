183e084200f233c1dfe066cf50a6dd000bd337ce
sung <sungwonchung3@gmail.com>
2025-11-25 11:29:53 -0800

    iterate on nested workflows

--- workflow_registry/another.py
+++ workflow_registry/another.py
@@ -3,7 +3,7 @@ from pydantic_ai.durable_exec.dbos import DBOSAgent
 from dbos import DBOS
 
 # @DBOS.step()
-def get_weather() -> str:  
+async def get_weather() -> str:  
     # Your I/O logic here  
     return 'rainy'  
 
@@ -19,5 +19,5 @@ dbos_agent = DBOSAgent(agent)
 async def main() -> None:
     # Note: DBOS.launch() would be needed here if running directly with python
     # but we are optimizing for CLI usage now.
-    result = await dbos_agent.run('What is the capital of Mexico and tell me the weather?')
-    print(result.output)
+    result = await get_weather()
+    print(result)
--- workflow_registry/example.py
+++ workflow_registry/example.py
@@ -1,16 +1,31 @@
 from pydantic_ai import Agent
 from pydantic_ai.durable_exec.dbos import DBOSAgent
+from dbos import DBOS
+
+@DBOS.step()
+async def get_weather() -> str:  
+    # Your I/O logic here  
+    return 'It is sunny today'
+
+@DBOS.step()
+async def get_mood() -> str:  
+    # Your I/O logic here  
+    return 'You are happy today'
 
 agent = Agent(
-    'gpt-5',
+    'gpt-5-nano',
     instructions="You're an expert in geography.",
     name='geography',  
+    tools=[get_weather],
 )
 
 dbos_agent = DBOSAgent(agent)  
 
+@DBOS.workflow() # this is a workflow that wraps dbos_agent as a nested workflow
 async def main() -> None:
     # Note: DBOS.launch() would be needed here if running directly with python
     # but we are optimizing for CLI usage now.
-    result = await dbos_agent.run('What is the capital of Mexico?')  
+    result = await dbos_agent.run('What is the capital of Mexico and the weather?')
+    mood = await get_mood()
     print(result.output)
+    print(mood)
